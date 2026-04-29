1b9e8277190856169fce5cda332486c9dce93330
NaveenSingh <naveensingh.mtp@gmail.com>
2026-04-29 23:54:31 +0530

    no message

--- src/constant.js
+++ src/constant.js
@@ -1,4 +1,17 @@
-export const normalize = (
+
+export const clamp = (
+    value,
+    min = 0,
+    max = 1
+) => {
+    if(min > max) {
+        [min,max] = [max,min];
+    }
+    return Math.max(min,Math.min(max,value))
+}
+
+
+export const normalize_old = (
     value,
     currentScaleMin,
     currentScaleMax,
@@ -9,5 +22,38 @@ export const normalize = (
 
     return (
         (newScaleMax - newScaleMin) * standardNormalization + newScaleMin
+    );
+}
+
+export const normalize = (
+  number,
+  currentScaleMin,
+  currentScaleMax,
+  newScaleMin = 0,
+  newScaleMax = 1
+) => {
+  const standardNormalization =
+    (number - currentScaleMin) / (currentScaleMax - currentScaleMin);
+
+  return (newScaleMax - newScaleMin) * standardNormalization + newScaleMin;
+};
+
+export const clampedNormalize = (
+    value,
+    currentScaleMin,
+    currentScaleMax,
+    newScaleMin = 0,
+    newSclaeMax = 1
+) => {
+    return clamp(
+         normalize(
+      value,
+      currentScaleMin,
+      currentScaleMax,
+      newScaleMin,
+      newScaleMax
+    ),
+    newScaleMin,
+    newScaleMax
     );
 }
